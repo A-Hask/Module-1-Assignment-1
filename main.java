b307221dc4f88b016e2c325efc9a394470ea2671
ahask93 <ahask93@gmail.com>
2024-06-08 19:21:12 -0400

    code with no errors

--- main.java
+++ main.java
@@ -2,16 +2,16 @@ public class main {
 
     /* Convert from feet to meters */
     public static double footToMeter(double foot) {
-        double meter;
-        meter = (0.305 * foot);
+        double meter = (0.305 * foot);
+        System.out.println("The measurement is " + meter + "meter.");
         return meter;
     }
 
 
     /* Convert from meters to feet */
     public static double meterToFoot(double meter){
-        double foot;
-        foot = (3.279 * meter);
+        double foot = (3.279 * meter);
+        System.out.println("The measurement is " + foot + "feet.");
         return foot;
     }
 }
