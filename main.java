c39580174df906c22d44324a7caab94f6e97f341
ahask93 <ahask93@gmail.com>
2024-06-08 17:26:33 -0400

    main class created

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+
+    /* Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        double meter;
+        meter = (0.305 * foot);
+        return meter;
+    }
+
+
+    /* Convert from meters to feet */
+    public static double meterToFoot(double meter){
+        double foot;
+        foot = (3.279 * meter);
+        return foot;
+    }
+}
+
